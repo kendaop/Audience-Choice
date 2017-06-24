edb01472a57ccf037d3ec323aff7c005e729f09e
kendaop <kendall.parks@live.com>
2017-06-24 16:39:14 -0500

    Re-did FilmTag Seeder

--- database/seeds/FilmSeeder.php
+++ database/seeds/FilmSeeder.php
@@ -17,10 +17,10 @@ class FilmSeeder extends Seeder
         if(empty($films)) {
             factory(Film::class, 30)->create();
         } else {
-            foreach ($films as $title => $description) {
+            foreach ($films as $title => $info) {
                 factory(App\Film::class)->create([
                     'title'       => $title,
-                    'description' => $description
+                    'description' => $info['description']
                 ]);
             }
         }
