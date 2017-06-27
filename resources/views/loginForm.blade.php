1d736b39549916b61baf3e4a69d7f9df056ce4f5
kendaop <kendall.parks@live.com>
2017-06-27 16:51:40 -0500

    CSS tweaks.

--- resources/views/loginForm.blade.php
+++ resources/views/loginForm.blade.php
@@ -51,7 +51,7 @@
         {{ csrf_field() }}
         <div class="form-group">
             <div class="row">
-                <div class="col-xs-offset-1 col-sm-offset-2 col-md-offset-3">
+                <div class="col-xs-offset-1 col-sm-offset-3 col-md-offset-3">
                     <label for="accessCodeInput">Access Code</label>
                 </div>
             </div>
